7d5b93af8484065d116fdc806f550ca6af253582
Trevor Bedson <trevobedson@gmail.com>
2025-11-23 00:15:15 -0500

    working more on fetcher

--- fetcher/src/index.ts
+++ fetcher/src/index.ts
@@ -2,15 +2,20 @@ import 'dotenv/config';
 import { Client } from "ftcmetrics-api";
 
 (async () => {
+
     const client = new Client(
         process.env.FTC_API_USERNAME as string, 
         process.env.FTC_API_TOKEN as string,
         process.env.FTC_PROD_URL
     )
 
-    while(true) {
-        const data = await client.ping()
-        console.log(data)
-        await new Promise((res) => setTimeout(res, 5000))
+    for(let i = 0; i < 29; i++) {
+        const data = await client.teams(2025, i)
     }
+
+    // while(true) {
+    //     const data = await client.ping()
+    //     console.log(data)
+    //     await new Promise((res) => setTimeout(res, 5000))
+    // }
 })();
